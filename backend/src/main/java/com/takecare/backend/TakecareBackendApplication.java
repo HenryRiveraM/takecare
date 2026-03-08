a4a5dea7a88c9c206182fd60f24b969b6e0a920b
ASalinasVe <alejandra.salinas@ucb.edu.bo>
2026-03-08 14:19:57 -0400

    Feat- Implementacion y configuracion de backend con bd

---
+++ backend/src/main/java/com/takecare/backend/TakecareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.takecare.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakecareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakecareBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/takecare/backend/TakecareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takecare.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakecareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
