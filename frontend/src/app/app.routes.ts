f4999a10d3be915dbe87b5fc5eb70f176b90596a
Valeria Gutierrez <valeria.gutierrez@ucb.edu.bo>
2026-03-10 18:38:38 -0400

    feat: Formulario de registro de especialista

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+// Importamos tu nuevo componente (ajusta la ruta si es necesario)
+import { RegisterSpecialistComponent } from './modules/auth/pages/register-specialist/register-specialist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Esta es la ruta para entrar a tu formulario
+  { 
+    path: 'registro-especialista', 
+    component: RegisterSpecialistComponent 
+  },
+  
+  // Si tienes una página de inicio, se vería así:
+  // { path: '', component: HomeComponent },
+];
---
+++ frontend/src/app/modules/auth/pages/register-specialist/register-specialist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-specialist',
+  imports: [],
+  templateUrl: './register-specialist.component.html',
+  styleUrl: './register-specialist.component.css'
+})
+export class RegisterSpecialistComponent {
+
+}
