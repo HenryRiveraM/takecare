4ec422e72008a887af979884b70ca2579382ce03
Isabella-12bit <isabella.montes@ucb.edu.bo>
2026-03-15 02:18:15 -0400

    Fix: Restaurando carpeta especialidades y arreglando la ubicación de especialistas

--- backend/src/main/java/com/takecare/backend/specialists/repository/SpecialistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.takecare.backend.specialities.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.takecare.backend.specialities.model.Specialist;
-
-public interface SpecialistRepository 
-        extends JpaRepository<Specialist, Long> {
-}
