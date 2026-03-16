6908463d8c8e05036a117f417dc92fa48943bf37
Valeria Gutierrez <valeria.gutierrez@ucb.edu.bo>
2026-03-16 03:00:22 -0400

    feat: interfaz de recuperacion de contraseña

---
+++ frontend/src/app/components/forgot-password/forgot-password.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Para el *ngIf
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router'; // Para el routerLink
+
+@Component({
+  selector: 'app-forgot-password',
+  standalone: true, // Esto indica que es versión nueva
+  imports: [CommonModule, ReactiveFormsModule, RouterModule], // ¡Importante!
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.css']
+})
+export class ForgotPasswordComponent {
+  recoveryForm: FormGroup;
+  submitted = false;
+  isLoading = false;
+
+  constructor(private fb: FormBuilder) {
+    this.recoveryForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  // Getter para facilitar acceso en HTML
+  get f() { return this.recoveryForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.recoveryForm.invalid) return;
+
+    this.isLoading = true;
+    
+    // Aquí irá la HU04-BE (Llamada al servicio)
+    console.log('Enviando correo a:', this.recoveryForm.value.email);
+    
+    // Simulamos respuesta del servidor
+    setTimeout(() => {
+      this.isLoading = false;
+      alert('Si el correo existe, recibirás un enlace pronto.');
+    }, 2000);
+  }
+}
