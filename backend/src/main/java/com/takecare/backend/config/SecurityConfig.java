34d69b69caf990aea35ddc38bece4fe1472816fc
Isabella-12bit <isabella.montes@ucb.edu.bo>
2026-03-16 23:32:35 -0400

    Fix: Desactivando guardia de seguridad

--- backend/src/main/java/com/takecare/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/takecare/backend/config/SecurityConfig.java
@@ -3,13 +3,15 @@ package com.takecare.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    // ESTO ES LO QUE NECESITAS PARA CIFRAR
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -17,18 +19,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http
             .csrf(csrf -> csrf.disable())
-            .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/v1/users/register/**").permitAll()
-                .requestMatchers("/api/v1/auth/**").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll() 
             )
-            .httpBasic(basic -> basic.disable())
-            .formLogin(login -> login.disable());
+            .formLogin(login -> login.disable())
+            .httpBasic(basic -> basic.disable()); 
 
         return http.build();
     }
